63abcce9de947d22d218a7dad0cd0551ed214ebf
dylanjsa90 <dylanjsanders1@gmail.com>
2016-12-05 16:33:19 -0800

    added add and subtract methods to letterInventory

--- LetterInventory.java
+++ LetterInventory.java
@@ -1,38 +1,38 @@
 
-public class LetterInventory {
+public class LetterInventory{
   private int size;
   private int[] inventory;
   private String initialInput;
 
-  public static final INT INVENTORY_SIZE = 26;
+  public static final int INVENTORY_SIZE = 26;
 
   // Letter inventory constructor, takes a string and inventories the number of each letter
-  public LetterInventory(String data) {
+  public LetterInventory(String data){
     initialInput = data;
     data = data.toLowerCase();
     for (int i = 0; i < data.length(); i++) {
-      if (Character.isLetter(data.charAt(i))) {
+      if (Character.isLetter(data.charAt(i)))
         inventory[data.charAt(i) - 97]++;  
-      }
+      
     }
   }
 
   // Returns the number of characters in the inventory
-  public int size() {
+  public int size(){
     return size;
   }
 
   // Returns the original input string
-  public string getOriginal() {
+  public String getOriginal(){
     return initialInput;
   }
 
   // Returns a whether the inventory is empty (true) or not (false)
-  public boolean isEmpty() {
+  public boolean isEmpty(){
     return size == 0;
   }
 
-  public int get(char letter) {
+  public int get(char letter){
     if (!Character.isLetter(letter))
       throw new IllegalArgumentException("Is not a letter: " + letter);
     return inventory[Character.toLowerCase(letter) - 'a'];  
@@ -48,11 +48,29 @@ public class LetterInventory {
     return result + "]";
   }
 
-  public void set(char letter, int value) {
+  public void set(char letter, int value){
     if (!Character.isLetter(letter) || value < 0)
       throw new IllegalArgumentException("Not a valid letter: " + letter + ", Or not valid value: " + value);
+    
     size += value - inventory[Character.toLowerCase(letter) - 'a'];
     inventory[Character.toLowerCase(letter) - 'a'] = value;
   }
 
+  public LetterInventory add(LetterInventory other) {
+    LetterInventory sum = new LetterInventory("");
+    for (int i = 0; i < INVENTORY_SIZE; i++)
+      sum.inventory[i] = this.inventory[i] + other.inventory[i];
+    sum.size = this.size + other.size;
+    return sum; 
+  }
+  public LetterInventory subtract(LetterInventory other){
+    LetterInventory result = new LetterInventory("");
+    for (int i = 0; i < INVENTORY_SIZE; i++){
+      result.inventory[i] = this.inventory[i] - other.inventory[i];
+      if (result.inventory[i] < 0)
+        return null;
+      result.size += result.inventory[i];
+    }
+    return result;
+  }
 }
